1ba80bd8a7fb65cd8949ac778abc7f3902365ec4
pvq2k2 <vanquyet2392002@gmail.com>
2022-10-19 00:24:00 +0700

    XTTN2-10 [Server] Middlewares inside Express

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,16 @@ app.get('/', (req, res) => {
     res.send("<h1>Hello I am from you backend server</h1>");
 })
 
+app.post('/sign-in', (req, res, next) => {
+    const {email, password} = req.body;
+    if (!email || !password) return res.json({ error: 'Email/password missing!' });
+    next();
+},
+    (req, res) => {
+        res.send("<h1>Hello I am from you backend server</h1>");
+    }
+);
+
 app.listen(8000, () => {
     console.log("The port is listening on port 8000");
 })
