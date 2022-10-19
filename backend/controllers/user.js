4ac9091dc997d28590b254ea1c32ddf818b3a4bc
Hoang Nam <namhoangde2002@gmail.com>
2022-10-19 19:15:15 +0700

    XTTN2-17 [Server] Resend Email Verification Token (OTP)

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,6 +2,8 @@ const EmailVerificationToken = require('../models/emailVerificationToken');
 const User = require('../models/user');
 const nodemailer = require('nodemailer');
 const { isValidObjectId } = require('mongoose');
+const { sendError } = require("../utils/helper");
+
 
 exports.create = async (req, res) => {
     const { name, email, password } = req.body;
@@ -89,3 +91,59 @@ exports.verifyEmail = async (req, res) => {
 
 
 }
+
+exports.resendEmailVerificationToken = async (req, res) => {
+    const { userId } = req.body;
+
+    const user = await User.findById(userId);
+    if (!user) return sendError(res, "user not found!");
+
+    if (user.isVerified)
+        return sendError(res, "This email id is already verified!");
+
+    const alreadyHasToken = await EmailVerificationToken.findOne({
+        owner: userId,
+    });
+    if (alreadyHasToken)
+        return sendError(res, "Only after one hour you can request for another token!");
+
+    // tạo otp 6 số
+    let OTP = ''
+    for (let i = 0; i <= 5; i++) {
+        const randomVal = Math.round(Math.random() * 9)
+        OTP += randomVal
+    }
+
+    // lưu otp vào db
+    const newEmailVerificationToken = new EmailVerificationToken({
+        owner: user._id,
+        token: OTP,
+    });
+
+    await newEmailVerificationToken.save();
+
+    // gửi otp cho user
+
+    var transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "522b6e2a39f1c1",
+            pass: "c687d8862cd6b6"
+        }
+    });
+    transport.sendMail({
+        from: "verification@reviewapp.com",
+        to: user.email,
+        subject: "Email Verification",
+        html: `
+        <p>Your verification OTP</p>
+        <h1>${OTP}</h1>
+  
+      `,
+    });
+
+    res.json({
+        message: "New OTP has been sent to your registered email accout.",
+    });
+};
