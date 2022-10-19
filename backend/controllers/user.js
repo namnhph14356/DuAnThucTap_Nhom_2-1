3466cb1009041651d0e74b91870ba8354f5a3c25
Hoang Nam <namhoangde2002@gmail.com>
2022-10-19 18:36:32 +0700

    XTTN2-16 [Server] Comparing Hashed OTP to Verify Email Verification Token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,7 +7,9 @@ exports.create = async (req, res) => {
     const { name, email, password } = req.body;
 
     const oldUser = await User.findOne({ email });
-    if (oldUser) return res.status(401).json({ error: "This email is already in use!" });
+
+    if (oldUser) return sendError(res, "This email is already in use!");
+
     const newUser = new User({ name, email, password });
     await newUser.save();
 
@@ -48,20 +50,42 @@ exports.create = async (req, res) => {
     res.status(201).json({ message: 'Please verify your email. OTP has been send to your Email' });
 }
 
-exports.verifyEmail = async (req,res) => {
-    const {userId, OTP} = req.body; 
+exports.verifyEmail = async (req, res) => {
+    const { userId, OTP } = req.body;
 
-    if(!isValidObjectId(userId)) return res.json({error: "Invalid User"});
+    if (!isValidObjectId(userId)) return res.json({ error: "Invalid User" });
 
     const user = await User.findById(userId)
-    if(!user) return res.json({error: "user not found"});
-    
-    if(user.isVerified) return res.json({error: "user is already verifiled"});
+    if (!user) return res.json({ error: "user not found" });
 
-    const token = await EmailVerificationToken.findOne({ owner: userId})
-    if(!token) return res.json({error: "token not found "});
+    if (user.isVerified) return res.json({ error: "user is already verifiled" });
 
+    const token = await EmailVerificationToken.findOne({ owner: userId })
+    if (!token) return res.json({ error: "token not found " });
 
+    const isMatched = await token.compaireToken(OTP)
+    if (!isMatched) return res.json({ error: "Please submit a valid OTP" });
+
+    user.isVerified = true;
+    await user.save()
+
+    await EmailVerificationToken.findByIdAndDelete(token._id)
+
+    var transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "522b6e2a39f1c1",
+            pass: "c687d8862cd6b6"
+        }
+    });
+    transport.sendMail({
+        from: 'verification@reviewapp.com',
+        to: user.email,
+        subject: 'Wellcom Email',
+        html: "<h1>Wellcome to our app and thanks for choosing us. </h1>"
+    })
+    res.json({ message: 'your email is verified' })
 
 
 }
--- backend/models/user.js
+++ backend/models/user.js
@@ -24,10 +24,11 @@ const userSchema = mongoose.Schema({
     },
 })
 
-userSchema.pre('save', async function (next) {
-    if (this.isModified('password')) {
-        this.password = bcrypt.hash(this.password, 10);
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
     }
+
     next();
-})
+});
 module.exports = mongoose.model("User", userSchema);
