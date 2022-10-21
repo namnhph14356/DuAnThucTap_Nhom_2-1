a8b247a71035ad78655ff3845cfc4b5ee30767f8
Hoang Nam <namhoangde2002@gmail.com>
2022-10-21 17:46:03 +0700

    XTTN2-26 [Server] Async Await Error Handling

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,31 @@
-const express =  require("express");
+const express = require("express");
+require("express-async-errors");
 const morgan = require("morgan");
+const { errorHandler } = require("./middlewares/error");
 require("dotenv").config();
-const userRouter = require("./routes/user");
 require("./db");
+const userRouter = require("./routes/user");
 
 const app = express();
 app.use(express.json());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 app.use("/api/user", userRouter);
 
+app.use(errorHandler);
 
-// app.post('/sign-in', (req, res, next) => {
-//     const {email, password} = req.body;
-//     if (!email || !password) return res.json({ error: 'Email/password missing!' });
+// app.post(
+//   "/sign-in",
+//   (req, res, next) => {
+//     const { email, password } = req.body;
+//     if (!email || !password)
+//       return res.json({ error: "email/password missing!" });
 //     next();
-// },
-//     (req, res) => {
-//         res.send("<h1>Hello I am from you backend server</h1>");
-//     }
+//   },
+//   (req, res) => {
+//     res.send("<h1>Hello I am about page</h1>");
+//   }
 // );
 
 app.listen(8000, () => {
-    console.log("The port is listening on port 8000");
-})
+  console.log("the port is listening on port 8000");
+});
--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -1,9 +1,9 @@
 const { check, validationResult } = require("express-validator");
 
 exports.userValidtor = [
-    check("name").trim().not().isEmpty().withMessage("Name is missing!"),
-    check("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
-    check("password")
+  check("name").trim().not().isEmpty().withMessage("Name is missing!"),
+  check("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
+  check("password")
     .trim()
     .not()
     .isEmpty()
@@ -13,24 +13,25 @@ exports.userValidtor = [
 ];
 
 exports.validatePassword = [
-    check("newPassword")
+  check("newPassword")
     .trim()
     .not()
     .isEmpty()
     .withMessage("Password is missing!")
     .isLength({ min: 8, max: 20 })
     .withMessage("Password must be 8 to 20 characters long!"),
-]
+];
+
+exports.signInValidator = [
+  check("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
+  check("password").trim().not().isEmpty().withMessage("Password is missing!"),
+];
 
 exports.validate = (req, res, next) => {
-    const error = validationResult(req).array();
-    if (error.length) {
-        return res.json({ error: error[0].msg});
-    }
-    console.log(error);
-    next();
-}
-exports.signInValidator = [
-    check("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
-    check("password").trim().not().isEmpty().withMessage("Password is missing!"),
-  ];
+  const error = validationResult(req).array();
+  if (error.length) {
+    return res.json({ error: error[0].msg });
+  }
+
+  next();
+};
