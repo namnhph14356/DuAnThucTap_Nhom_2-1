551b2e18197cdbc20dc5c14126f962d13dd95bdf
Hoang Nam <namhoangde2002@gmail.com>
2022-10-22 12:58:51 +0700

    XTTN2-48 [Client] Different Color for Light & Dark Mode

--- frontend/src/components/form/FormInput.jsx
+++ frontend/src/components/form/FormInput.jsx
@@ -3,22 +3,22 @@ import React from 'react'
 export default function FormInput({ name, label, placeholder, ...rest }) {
   return (
     <div className="flex flex-col-reverse">
-    <input
-      id={name}
-      name={name}
-      className="bg-transparent rounded border-2 
-    border-dark-subtle w-full text-lg outline-none 
-    focus:border-white p-1 text-white peer transition"
-      placeholder={placeholder}
-      {...rest}
-    />
-    <label
-      className="font-semibold text-dark-subtle 
-      peer-focus:text-white transition self-start"
-      htmlFor={name}
-    >
-      {label}
-    </label>
-  </div>
+      <input
+        id={name}
+        name={name}
+        className="bg-transparent rounded border-2 
+    dark:border-dark-subtle border-light-subtle w-full text-lg outline-none 
+    dark:focus:border-white focus:border-primary p-1 dark:text-white peer transition"
+        placeholder={placeholder}
+        {...rest}
+      />
+      <label
+        className="font-semibold dark:text-dark-subtle text-light-subtle
+      dark:peer-focus:text-white peer-focus:text-primary transition self-start"
+        htmlFor={name}
+      >
+        {label}
+      </label>
+    </div>
   )
 }
