74c8b3ed09106195c5d8dd8d52e771b864355a9b
thilv43 <thilv44443333@gmail.com>
2022-10-24 16:36:45 +0700

    XTTN2-67 [Client] Making Request to is-auth

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -34,3 +34,19 @@ export const signInUser = async (userInfo) => {
         return { error: error.message || error };
     }
 };
+export const getIsAuth = async (token) => {
+    try {
+        const { data } = await client.get("/user/is-auth", {
+            Headers: {
+                Authorization: 'Bearer' + token, 
+                accept: 'application/json',
+            }
+        });
+        return data;
+    } catch (error) {
+        const { response } = error;
+        if (response?.data) return response.data;
+
+        return { error: error.message || error };
+    }
+};
