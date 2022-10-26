9b5cc44276248a0a623ddb8c85a6b08790036558
thilv43 <thilv44443333@gmail.com>
2022-10-26 14:32:44 +0700

    XTTN2-80 [Client] Conditionally Rendering Resend Email Verification

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 export default function Home() {
   const {authInfo} = useAuth()
-  console.log(authInfo);
+  const {isLoggedIn} = authInfo;
+  const isVerified = authInfo.profile?.isVerified; 
+
+
   return (
     <Container>
-      <p className='text-lg text-center bg-blue-50 p-2'>It looks like you haven't verified your account, {''}
+      {isLoggedIn && !isVerified ? (<p className='text-lg text-center bg-blue-50 p-2'>It looks like you haven't verified your account, {''}
         <button className='text-blue-500 font-semibold hover:underline'>Click here to verify your account.</button>
-      </p>
+      </p> ) : null}
     </Container>
   )
 }
