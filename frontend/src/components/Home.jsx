eedaef1b3cb4f9fe7895684438c69ab2fdcec93e
thilv43 <thilv44443333@gmail.com>
2022-10-26 14:25:57 +0700

    XTTN2-79 [Client] Component to Resend Email Verification

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export default function Home() {
+  const {authInfo} = useAuth()
+  console.log(authInfo);
   return (
-    <div>Home</div>
+    <Container>
+      <p className='text-lg text-center bg-blue-50 p-2'>It looks like you haven't verified your account, {''}
+        <button className='text-blue-500 font-semibold hover:underline'>Click here to verify your account.</button>
+      </p>
+    </Container>
   )
 }
