21c5c8e1cc0b084473c732048f4b523181c9f144
thilv43 <thilv44443333@gmail.com>
2022-10-26 15:11:55 +0700

    XTTN2-83 [Client] Resending Email Verification Token

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -83,3 +83,14 @@ export const resetPassword = async (passwordInfo) => {
         return { error: error.message || error };
     }
 };
+export const resendEmailVerificationToken = async (userId) => {
+    try {
+        const { data } = await client.post("/user/resend-email-verfication-token", userId);
+        return data;
+    } catch (error) {
+        const { response } = error;
+        if (response?.data) return response.data;
+
+        return { error: error.message || error };
+    }
+};
