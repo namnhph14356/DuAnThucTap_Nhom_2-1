80d0383ea71a1de35790274f734306218785422e
pvq2k2 <vanquyet2392002@gmail.com>
2022-10-28 10:48:27 +0700

    XTTN2-126 [Client] Rendering Create Options

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -3,15 +3,20 @@ import { AiOutlinePlus } from 'react-icons/ai'
 
 export default function Dashboard() {
   return (
-    <div className='flex item-center justify-between'>
+    <div className='flex item-center justify-between relative'>
       <input type="text" className='border-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary
         dark:text-white transition bg-transparent rounded text-lg p-1 outline-none' placeholder='Search Movies.....' />
       
       <button className='flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80
       transition font-semibold border-2 rounded text-lg px-3 py-1'>
-        <span>Creat</span>
+        <span>Create</span>
         <AiOutlinePlus/>
       </button>
+
+      <div className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded">
+        <button className="dark:text-white text-secondary hover:opacity-80 transition">Add Movie</button>
+        <button className="dark:text-white text-secondary hover:opacity-80 transition">Add Actor</button>
+      </div>
     </div>
   )
 }
