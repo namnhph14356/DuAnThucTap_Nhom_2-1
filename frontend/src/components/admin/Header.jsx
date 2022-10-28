2805f2d6477ddf3c7dde565402e8a2b7e9c26933
pvq2k2 <vanquyet2392002@gmail.com>
2022-10-28 15:39:01 +0700

    XTTN2-132 [Client] Closing Options With Animation

--- frontend/src/components/admin/Header.jsx
+++ frontend/src/components/admin/Header.jsx
@@ -39,10 +39,12 @@ export default function Header() {
 
 const CreateOptions = ({ visible, onClose }) => {
     const container = useRef();
-    // const containerID = "options-container";
+    const containerID = "options-container";
     useEffect(() => {
         const handleClose = (e) => {
             if (!visible) return;
+            const { parentElement, id } = e.target;
+            if (parentElement.id === containerID || id === containerID) return;
             container.current.classList.remove('animate-scale');
             container.current.classList.add('animate-scale-reverse');
         };
@@ -53,7 +55,14 @@ const CreateOptions = ({ visible, onClose }) => {
     }, [visible]);
   if (!visible) return null;
   return (
-    <div ref={container} className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded animate-scale">
+    <div ref={container}
+    id={containerID}
+    className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded animate-scale"
+    onAnimationEnd={(e) => {
+      if (e.target.classList.contains('animate-scale-reverse')) onClose();
+      e.target.classList.remove('animate-scale');
+    }}
+    >
       <Option>Add Movie</Option>
       <Option>Add Actor</Option>
     </div>
