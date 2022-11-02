d4f890d7fd356b385b585c45dc515ac00691fd4c
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-02 10:43:05 +0700

    XTTN2-166 [Client] Reset Selection Index

--- frontend/src/components/LiveSearch.jsx
+++ frontend/src/components/LiveSearch.jsx
@@ -48,6 +48,7 @@ export default function LiveSearch() {
   };
   const handleOnBlur = () => {
     setDisplayedSearch(false);
+    setFocusedIndex(-1);
   };
   const handleKeyDown = ({ key }) => {
     let nextCount;
