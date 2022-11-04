d554e13be2a5e747326fc97dd38450908cf8fd23
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-04 10:51:02 +0700

    XTTN2-179 [Client] Adding Styles to Writers Profile

--- frontend/src/components/modals/WritersModal.jsx
+++ frontend/src/components/modals/WritersModal.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
 import ModalContainer from './ModalContainer'
 
 export default function WritersModal({profile = [], visible, onClose}) {
   return (
     <ModalContainer onClose={onClose} visible={visible}>
+        <div className="space-y-2">
         {profile.map(({id, name, avatar}) => {
             return (
-                <div key={id} className="flex">
-                    <img src={avatar} alt={name} />
-                    <p>{name}</p>
+                <div key={id} className="flex space-x-3">
+                    <img className="w-16 h-16 rounded object-cover" src={avatar} alt={name} />
+                    <p className="w-full font-semibold dark:text-white text-primary">{name}</p>
+                    <button className='dark:text-white text-primary hover:opacity-80 transition p-2'><AiOutlineClose/></button>
                 </div>
             )
         })}
+        </div>
     </ModalContainer>
   )
 }
