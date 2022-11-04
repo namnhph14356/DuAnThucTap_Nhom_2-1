693a62b6bdd5f47844895cef6bd6fc539386ef47
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-04 11:04:31 +0700

    XTTN2-180 [Client] Optionally Remove Container inside Modal

--- frontend/src/components/modals/WritersModal.jsx
+++ frontend/src/components/modals/WritersModal.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import { AiOutlineClose } from 'react-icons/ai'
-import ModalContainer from './ModalContainer'
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import ModalContainer from "./ModalContainer";
 
-export default function WritersModal({profile = [], visible, onClose}) {
+export default function WritersModal({ profile = [], visible, onClose }) {
   return (
-    <ModalContainer onClose={onClose} visible={visible}>
-        <div className="space-y-2">
-        {profile.map(({id, name, avatar}) => {
-            return (
-                <div key={id} className="flex space-x-3">
-                    <img className="w-16 h-16 rounded object-cover" src={avatar} alt={name} />
-                    <p className="w-full font-semibold dark:text-white text-primary">{name}</p>
-                    <button className='dark:text-white text-primary hover:opacity-80 transition p-2'><AiOutlineClose/></button>
-                </div>
-            )
+    <ModalContainer ignoreContainer onClose={onClose} visible={visible}>
+      <div className="space-y-2 dark:bg-primary bg-white rounded max-w-[45rem] max-h-[40rem] overflow-auto p-2 custom-scroll-bar">
+        {profile.map(({ id, name, avatar }) => {
+          return (
+            <div key={id} className="flex space-x-3">
+              <img
+                className="w-16 h-16 aspect-square rounded object-cover"
+                src={avatar}
+                alt={name}
+              />
+              <p className="w-full font-semibold dark:text-white text-primary">
+                {name}
+              </p>
+              <button className="dark:text-white text-primary hover:opacity-80 transition p-2">
+                <AiOutlineClose />
+              </button>
+            </div>
+          );
         })}
-        </div>
+      </div>
     </ModalContainer>
-  )
+  );
 }
