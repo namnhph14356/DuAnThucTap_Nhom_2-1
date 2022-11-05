c359e24ed5c6017ec81b888dcca930016f357bf6
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-05 21:18:26 +0700

    XTTN2-194 [Client] Selecting & Rendering Poster

--- frontend/src/components/PosterSelector.jsx
+++ frontend/src/components/PosterSelector.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
+import React from "react";
 
-const commonPosterUI = "flex justify-center items-center border border-dashed rounded aspect-video dark:border-dark-subtle border-light-subtle";
+const commonPosterUI =
+  "flex justify-center items-center border border-dashed rounded aspect-video dark:border-dark-subtle border-light-subtle";
 
-
-export default function PosterSelector  ({name, selectedPoster}) {
-    return (
-        <div>
-            <input id={name} type="file" hidden />
-            <label htmlFor='name'>
-                {selectedPoster ? <img className={commonPosterUI + 'object-cover'} src={selectedPoster} alt='' /> : <PosterUI />}
-            </label>
-        </div>
-    )
+export default function PosterSelector({
+  name,
+  accept,
+  selectedPoster,
+  onChange,
+}) {
+  return (
+    <div>
+      <input
+        accept={accept}
+        onChange={onChange}
+        name={name}
+        id={name}
+        type="file"
+        hidden
+      />
+      <label htmlFor={name}>
+        {selectedPoster ? (
+          <img
+            className={commonPosterUI + "object-cover"}
+            src={selectedPoster}
+            alt=""
+          />
+        ) : (
+          <PosterUI />
+        )}
+      </label>
+    </div>
+  );
 }
 
 const PosterUI = () => {
-    return <div className={commonPosterUI}>
-    <span className="dark:text-dark-subtle text-light-subtle">Select Poster</span>
+  return (
+    <div className={commonPosterUI}>
+      <span className="dark:text-dark-subtle text-light-subtle">
+        Select Poster
+      </span>
     </div>
-}
+  );
+};
