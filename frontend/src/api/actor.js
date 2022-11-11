38156b23952ebab80382dde92add507e3c08c84f
Hoang Nam <namhoangde2002@gmail.com>
2022-11-12 01:18:18 +0700

    XTTN2-219 [Client] Little Refactor Again

--- frontend/src/api/actor.js
+++ frontend/src/api/actor.js
@@ -1,19 +1,17 @@
+import { cathError, getToken } from "../utils/helper";
 import client from "./client";
 
 export const createActor = async (formData) => {
-    const token = localStorage.getItem("auth-token");
+    const token = getToken()
     try {
         const { data } = await client.post("/actor/create", formData, {
-        headers: {
-            authorization: "Bearer " + token,
-            "Content-Type": "multipart/form-data",
+            headers: {
+                authorization: "Bearer " + token,
+                "Content-Type": "multipart/form-data",
             },
         });
         return data;
-        } catch (error) {
-        const { response } = error;
-        if (response?.data) return response.data;
-    
-        return { error: error.message || error };
-        }
+    } catch (error) {
+        return cathError(error)
+    }
 };
--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -3,3 +3,11 @@ export const isValidEmail = (email) =>{
     const isValid = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/;
     return isValid.test(email);
 }
+export const getToken = () => localStorage.getItem("auth-token");
+
+export const cathError = (error) => {
+    const { response } = error;
+    if (response?.data) return response.data;
+
+    return { error: error.message || error };
+}
