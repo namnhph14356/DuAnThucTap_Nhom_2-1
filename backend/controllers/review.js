e14548897ac25c999d0ec8800fc02857ccf61024
thanhtra17 <ttra170302@gmail.com>
2022-11-12 13:13:12 +0700

    XTTN2-227 [Server] Adding New Review Part 1

--- backend/controllers/review.js
+++ backend/controllers/review.js
@@ -1,3 +1,22 @@
-exports.addReview = ( req, res ) =>{
-    
+const { isValidObjectId } = require("mongoose");
+const { sendError } = require("../utils/helper");
+const Movie = require("../models/movie")
+const Review = require("../models/review")
+
+exports.addReview = async ( req, res ) =>{
+    const {movieId} = req.params;
+    const {content, rating} = req.body;
+    const userId = req.user._id;
+
+
+    if(!isValidObjectId(movieId)) return sendError(res, "Invalid Movie!")
+
+    const movie = await  Movie.findOne({_id: movieId, status: "public"})
+    if(!movie) return sendError(res, "Movie not found!", 404)
+
+    const isAlreadyReviewed =  await Review.findOne({owner: userId, parentMovie: movie._id })
+    if(isAlreadyReviewed) return sendError(res, "Invalid request, review is already their!")
+
+    // creat and update review.
+
 }
