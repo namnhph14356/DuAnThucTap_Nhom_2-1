77efd8f2df6fbf7f1947406074781ef9cbec6be4
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-20 22:31:34 +0700

    XTTN2-280 [Client] Fetching Actors

--- frontend/src/api/actor.js
+++ frontend/src/api/actor.js
@@ -2,31 +2,49 @@ import { cathError, getToken } from "../utils/helper";
 import client from "./client";
 
 export const createActor = async (formData) => {
-    const token = getToken()
-    try {
-        const { data } = await client.post("/actor/create", formData, {
-            headers: {
-                authorization: "Bearer " + token,
-                "Content-Type": "multipart/form-data",
-            },
-        });
-        return data;
-    } catch (error) {
-        return cathError(error)
-    }
+  const token = getToken();
+  try {
+    const { data } = await client.post("/actor/create", formData, {
+      headers: {
+        authorization: "Bearer " + token,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return data;
+  } catch (error) {
+    return cathError(error);
+  }
 };
 
 export const searchActor = async (query) => {
-    const token = getToken()
-    try {
-        const { data } = await client(`/actor/search?name=${query}`, {
-            headers: {
-                authorization: "Bearer " + token,
-                "Content-Type": "multipart/form-data",
-            },
-        });
-        return data;
-    } catch (error) {
-        return cathError(error)
-    }
+  const token = getToken();
+  try {
+    const { data } = await client(`/actor/search?name=${query}`, {
+      headers: {
+        authorization: "Bearer " + token,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return data;
+  } catch (error) {
+    return cathError(error);
+  }
+};
+
+export const getActors = async (pageNo, limit) => {
+  const token = getToken();
+  try {
+    const { data } = await client(
+      `/actor/actors?pageNo=${pageNo}&limit=${limit}`,
+      {
+        headers: {
+          authorization: "Bearer " + token,
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    return cathError(error);
+  }
 };
