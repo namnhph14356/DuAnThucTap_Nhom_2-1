a80f2b9ae7a3eb45c320d28f8ee9400353c7608c
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-20 23:03:10 +0700

    XTTN2-282 [Client] Rendering the Actor Profiles

--- frontend/src/components/admin/Actors.jsx
+++ frontend/src/components/admin/Actors.jsx
@@ -1,17 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { BsTrash, BsPencilSquare } from "react-icons/bs";
 import { getActors } from "../../api/actor";
+import { useNotification } from "../../hooks";
+
+let pageNo = 0;
+const limit = 20;
 
 export default function Actors() {
+  const [actors, setActors] = useState([]);
+  const { updateNotification } = useNotification();
+
   const fetchActors = async () => {
-    const res = await getActors(0, 5);
-    console.log(res);
+    const { profiles, error } = await getActors(pageNo, limit);
+    if (error) return updateNotification("error", error);
+
+    setActors([...profiles]);
   };
 
   useEffect(() => {
     fetchActors();
   }, []);
-  return <div className="grid grid-cols-4 gap-3 my-5"></div>;
+  return (
+    <div className="grid grid-cols-4 gap-5 p-5">
+      {actors.map((actor) => {
+        return <ActorProfile profile={actor} key={actor.id} />;
+      })}
+    </div>
+  );
 }
 
 const ActorProfile = ({ profile }) => {
