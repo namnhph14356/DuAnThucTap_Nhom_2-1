ba1c649cf41e886cd93738d129cba4fa03616cd4
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-20 09:13:57 +0700

    XTTN2-270 [Client] Rendering 3 Containers in a Row

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -2,11 +2,25 @@ import React from "react";
 
 export default function Dashboard() {
   return (
-    <div className="bg-white shadow dark:bg-secondary p-5 rounded">
-      <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
-        Total Uploads
-      </h1>
-      <p className="text-xl text-primary dark:text-white">100</p>
+    <div className="grid grid-cols-3 gap-5 my-5">
+      <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded">
+        <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
+          Total Uploads
+        </h1>
+        <p className="text-xl text-primary dark:text-white">100</p>
+      </div>
+      <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded">
+        <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
+          Total Reviews
+        </h1>
+        <p className="text-xl text-primary dark:text-white">100</p>
+      </div>
+      <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded">
+        <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
+          Total Users
+        </h1>
+        <p className="text-xl text-primary dark:text-white">100</p>
+      </div>
     </div>
   );
 }
