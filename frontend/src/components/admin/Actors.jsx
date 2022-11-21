3b7e8a42d9759fe6fe2e5651d78d9d04cc8b4775
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-21 10:13:24 +0700

    XTTN2-284 [Client] The Working Next Button

--- frontend/src/components/admin/Actors.jsx
+++ frontend/src/components/admin/Actors.jsx
@@ -4,22 +4,27 @@ import { BsTrash, BsPencilSquare } from "react-icons/bs";
 import { getActors } from "../../api/actor";
 import { useNotification } from "../../hooks";
 
-let pageNo = 0;
+let currentPageNo = 0;
 const limit = 20;
 
 export default function Actors() {
   const [actors, setActors] = useState([]);
   const { updateNotification } = useNotification();
 
-  const fetchActors = async () => {
+  const fetchActors = async (pageNo) => {
     const { profiles, error } = await getActors(pageNo, limit);
     if (error) return updateNotification("error", error);
 
     setActors([...profiles]);
   };
 
+  const handleOnNextClick = () => {
+    currentPageNo += 1;
+    fetchActors(currentPageNo);
+  };
+
   useEffect(() => {
-    fetchActors();
+    fetchActors(currentPageNo);
   }, []);
   return (
     <div className="p-5">
@@ -39,6 +44,7 @@ export default function Actors() {
         <button
           type="button"
           className="text-primary dark:text-white hover:underline"
+          onClick={handleOnNextClick}
         >
           Next
         </button>
