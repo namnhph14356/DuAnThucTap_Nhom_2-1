e0bd8234b1d7359ba8eca30d41da43f9d0b66064
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-21 13:05:49 +0700

    XTTN2-291 [Client] Fetching and Rendering Movies

--- frontend/src/api/movie.js
+++ frontend/src/api/movie.js
@@ -2,34 +2,53 @@ import { cathError, getToken } from "../utils/helper";
 import client from "./client";
 
 export const uploadTrailer = async (formData, onUploadProgress) => {
-  const token = getToken()
+  const token = getToken();
   try {
     const { data } = await client.post("/movie/upload-trailer", formData, {
       headers: {
         authorization: "Bearer " + token,
         "Content-Type": "multipart/form-data",
       },
-      onUploadProgress: ({loaded, total}) => {
-        if (onUploadProgress) onUploadProgress(Math.floor((loaded / total) * 100));
-      }
+      onUploadProgress: ({ loaded, total }) => {
+        if (onUploadProgress)
+          onUploadProgress(Math.floor((loaded / total) * 100));
+      },
     });
     return data;
   } catch (error) {
-    return cathError(error)
+    return cathError(error);
   }
 };
 
 export const uploadMovie = async (formData) => {
-  const token = getToken()
+  const token = getToken();
   try {
     const { data } = await client.post("/movie/create", formData, {
       headers: {
         authorization: "Bearer " + token,
         "Content-Type": "multipart/form-data",
-      }
+      },
     });
     return data;
   } catch (error) {
-    return cathError(error)
+    return cathError(error);
+  }
+};
+
+export const getMovies = async (pageNo, limit) => {
+  const token = getToken();
+  try {
+    const { data } = await client(
+      `/movie/movies?pageNo=${pageNo}&limit=${limit}`,
+      {
+        headers: {
+          authorization: "Bearer " + token,
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    return cathError(error);
   }
 };
