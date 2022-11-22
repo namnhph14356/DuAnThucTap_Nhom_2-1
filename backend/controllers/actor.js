94f4c21e5ae889fe556aab580e43ade5d285e527
Hoang Nam <namhoangde2002@gmail.com>
2022-11-23 02:01:02 +0700

    XTTN2-310 [Client] Prevent From Empty Form Submission

--- backend/controllers/actor.js
+++ backend/controllers/actor.js
@@ -85,11 +85,11 @@ exports.removeActor = async (req, res) => {
 };
 
 exports.searchActor = async (req, res) => {
-  const { query } = req;
+  const { name } = req.query;
 
   // const result = await Actor.find({ $text: { $search: `"${query.name}"` } });
-  const result = await Actor.find({ name: {$regex: query.name, $options: "i"}, });
-
+  if(!name.trim()) return sendError(res, "Invalid request!")
+  const result = await Actor.find({ name: {$regex: name, $options: "i"}, });
 
   const actors = result.map((actor) => formatActor(actor));
 
