c697d3a60d2dae92615ba83c40a9eed443605638
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-24 01:22:01 +0700

    XTTN2-326 [Client] Movie Delete From Dashboard

--- frontend/src/components/LatestUploads.jsx
+++ frontend/src/components/LatestUploads.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
-import { getMovies } from "../api/movie";
+import { deleteMovie, getMovies } from "../api/movie";
 import { useNotification } from "../hooks";
+import ConfirmModal from "./modals/ConfirmModal";
 import MovieListItem from "./MovieListItem";
 
 const pageNo = 0;
@@ -9,6 +10,9 @@ const limit = 5;
 
 export default function LatestUploads() {
   const [movies, setMovies] = useState([]);
+  const [busy, setBusy] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
   const { updateNotification } = useNotification();
 
   const fetchLastestUploads = async () => {
@@ -18,19 +22,52 @@ export default function LatestUploads() {
     setMovies([...movies]);
   };
 
+  const handleOnDeleteClick = (movie) => {
+    setSelectedMovie(movie);
+    setShowConfirmModal(true);
+  };
+
+  const handleOnDeleteConfirm = async () => {
+    setBusy(true);
+    const { error, message } = await deleteMovie(selectedMovie.id);
+    setBusy(false);
+
+    if (error) return updateNotification("error", error);
+    updateNotification("success", message);
+    hideConfirmModal();
+  };
+
+  const hideConfirmModal = () => setShowConfirmModal(false);
+
   useEffect(() => {
     fetchLastestUploads();
   }, []);
   return (
-    <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded col-span-2">
-      <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
-        Recent Uploads
-      </h1>
-      <div className="space-y-3">
-        {movies.map((movie) => {
-          return <MovieListItem movie={movie} key={movie.id} />;
-        })}
+    <>
+      <div className="bg-white shadow dark:shadow dark:bg-secondary p-5 rounded col-span-2">
+        <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
+          Recent Uploads
+        </h1>
+        <div className="space-y-3">
+          {movies.map((movie) => {
+            return (
+              <MovieListItem
+                movie={movie}
+                key={movie.id}
+                onDeleteClick={() => handleOnDeleteClick(movie)}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
+      <ConfirmModal
+        title="Are you sure?"
+        subtitle="This action will remove this movie permanently!"
+        visible={showConfirmModal}
+        onCancel={hideConfirmModal}
+        onConfirm={handleOnDeleteConfirm}
+        busy={busy}
+      />
+    </>
   );
 }
