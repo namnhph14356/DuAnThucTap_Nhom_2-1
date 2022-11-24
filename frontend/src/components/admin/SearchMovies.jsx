5ffb75d413496e1653b52c70e13b27b49a8faa17
Hoang Nam <namhoangde2002@gmail.com>
2022-11-24 15:09:31 +0700

    XTTN2-331 [Client] Grabbing query from the URL

--- frontend/src/components/admin/SearchMovies.jsx
+++ frontend/src/components/admin/SearchMovies.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 export default function SearchMovies() {
-  return (
-    <div>SearchMovies</div>
-  )
+    const [searchParams] = useSearchParams()
+    const query = searchParams.get("title")
+
+    const searchMovies = (val) => {
+        console.log(val);
+    }
+    
+    useEffect(() => {
+        if (query.trim()) searchMovies(query)
+    }, [query])
+    return (
+        <div>SearchMovies</div>
+    )
 }
