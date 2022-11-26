220c38de96d7bb0b5ac1eee2301da310490f0b95
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-26 22:26:01 +0700

    XTTN2-342 [Client] Styling Movie Cards Part 1

--- frontend/src/components/user/TopRatedMovies.jsx
+++ frontend/src/components/user/TopRatedMovies.jsx
@@ -1,30 +1,41 @@
-import  React , {useState, useEffect} from 'react';
-import { getTopRatedMovies } from '../../api/movie';
-import { useNotification } from '../../hooks';
-import GridContainer from '../GridContainer';
+import React, { useState, useEffect } from "react";
+import { getTopRatedMovies } from "../../api/movie";
+import { useNotification } from "../../hooks";
+import GridContainer from "../GridContainer";
 
+export default function TopRateMovies() {
+  const [movies, setMovies] = useState([]);
+  const { updateNotification } = useNotification();
 
-export default function TopRateMovies  () {
-    const [movies, setMovies] = useState([]);
-    const {updateNotification} = useNotification();
-    
+  const fetchMovies = async () => {
+    const { error, movies } = await getTopRatedMovies();
+    if (error) return updateNotification("error", error);
 
-    const fetchMovies = async () => {
-        const {error, movies} = await getTopRatedMovies();
-        if(error) return updateNotification('error', error);
+    setMovies([...movies]);
+  };
 
-        setMovies([...movies]);
-    };
-    useEffect(() => {
-        fetchMovies();
-    }, []);
+  const trimTitle = (text = "") => {
+    if (text.length <= 20) return text;
+    return text.substring(0, 20) + "...";
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
-
-    return (
-        <GridContainer>
-            {movies.map((_, index) => {
-                return <div className='p-5 bg-red-200' key={index}> </div>
-            })}
-        </GridContainer>
-    );
-}; 
+  return (
+    <GridContainer>
+      {movies.map((movie) => {
+        return (
+          <div key={movie.id}>
+            <img
+              className="aspect-video object-cover"
+              src={movie.poster}
+              alt={movie.title}
+            />
+            <h1 title={movie.title}>{trimTitle(movie.title)}</h1>
+          </div>
+        );
+      })}
+    </GridContainer>
+  );
+}
