acb075edd3232a02d375b003c8e2f887e9f8789a
thilv43 <thilv44443333@gmail.com>
2022-11-27 10:50:37 +0700

    XTTN2-349 [Client] Fetching Latest Uploads

--- frontend/src/components/user/HeroSlidShow.jsx
+++ frontend/src/components/user/HeroSlidShow.jsx
@@ -1,10 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import { getLatestUploads } from '../../api/movie';
+import { useNotification } from '../../hooks';
 
 export default function HeroSlidShow() {
     const [slide, setSlide] = useState({});
+    const [movies, setMovies] = useState([]);
+
+    const {updateNotification} = useNotification();
+
+    const fetchLatesUPloads = async () => {
+        const {error, movies} = await getLatestUploads();
+        if(error) return updateNotification('error', error);
+
+        setMovies([...movies]);
+        setSlide(movies[0]);
+    }
+    useEffect(() => {
+        fetchLatesUPloads();
+    }, []);
+
     return (
         <div className='w-full flex'>
-            <div className='w-4/5 aspect-video bg-blue-300'>
+            <div className='w-4/5 aspect-video'>
                 <img src={slide.poster} alt="" />
             </div>
             <div className='w-1/5 aspect-video bg-red-300'></div>
