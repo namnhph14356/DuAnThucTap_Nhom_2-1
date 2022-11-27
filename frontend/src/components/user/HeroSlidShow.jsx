6e56c9d7a696181afb03f1834bd152a7dc439aaf
thilv43 <thilv44443333@gmail.com>
2022-11-27 21:58:59 +0700

    XTTN2-350 [Client] Slide Show Controllers

--- frontend/src/components/user/HeroSlidShow.jsx
+++ frontend/src/components/user/HeroSlidShow.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import {AiOutlineDoubleLeft, AiOutlineDoubleRight} from 'react-icons/ai'
 import { getLatestUploads } from '../../api/movie';
 import { useNotification } from '../../hooks';
 
@@ -21,10 +22,25 @@ export default function HeroSlidShow() {
 
     return (
         <div className='w-full flex'>
-            <div className='w-4/5 aspect-video'>
-                <img src={slide.poster} alt="" />
+            <div className='w-4/5 aspect-video relative'>
+                <img src={slide.poster} alt=""  />
+                <SlideShowController />
             </div>
             <div className='w-1/5 aspect-video bg-red-300'></div>
         </div> 
+    )
+}
+
+const SlideShowController = ({onNextClick, onPrevClick}) => {
+    const btnClass = "bg-primary border-white rounded border-2 text-white text-xl p-2 outline-none";
+    return (
+        <div className='absolute top-1/2 -translate-y-1/2 w-full flex items-center justify-between px-2'>
+                    <button onClick={onPrevClick} className={btnClass} type='button'>
+                        <AiOutlineDoubleLeft />
+                    </button>
+                    <button onClick={onNextClick} className={btnClass} type='button'>
+                        <AiOutlineDoubleRight />
+                    </button>
+        </div>
     )
 }
