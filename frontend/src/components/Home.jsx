1622f9f31b7a3ba7fbde4398a55ca41e4085c3b8
thilv43 <thilv44443333@gmail.com>
2022-11-28 21:37:06 +0700

    XTTN2-356 [Client] The Problem With Slider

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -9,7 +9,7 @@ import HeroSlidShow from "./user/HeroSlidShow";
 export default function Home() {
   return (
     <div className="dark:bg-primary bg-white min-h-screen ">
-      <Container>
+      <Container className='px-2 xl:p-0 '>
         <NotVerified />
         {/* sline*/}
         <HeroSlidShow />
