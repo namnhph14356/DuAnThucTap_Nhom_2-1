cc33a126e0062ddc9b996ea21ec0f3ffbfd42538
Hoang Nam <namhoangde2002@gmail.com>
2022-11-30 01:15:22 +0700

    XTTN2-390 [Client] Adding New Review

--- frontend/src/components/modals/AddRatingModal.jsx
+++ frontend/src/components/modals/AddRatingModal.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { addReview } from '../../api/review'
+import { useNotification } from '../../hooks'
 import RatingForm from '../form/RatingForm'
 import ModalContainer from './ModalContainer'
 
 
 export default function AddRatingModal({ visible, onClose }) {
-    const handleSubmit = (data) => {
-        console.log(data);
+    const { movieId } = useParams()
+    const { updateNotification } = useNotification();
+
+    const handleSubmit = async (data) => {
+        const { error, message } = await addReview(movieId, data)
+        if (error) return updateNotification("error", error)
+        updateNotification("success", message)
+        onClose()
     }
     return (
         <ModalContainer visible={visible} onClose={onClose} ignoreContainer>
