7f268f3222f7761d30d72d337d9d05f4de6daa4c
Hoang Nam <namhoangde2002@gmail.com>
2022-11-29 23:25:25 +0700

    XTTN2-376 [Client] Rendering Cast Language & Other Important Things

--- frontend/src/components/user/SingleMovie.jsx
+++ frontend/src/components/user/SingleMovie.jsx
@@ -12,6 +12,10 @@ const convertReviewCount = (count) => {
   return parseInt(count / 1000).toFixed(2) + 'k'
 }
 
+const convertDate = (date = "") => {
+  return date.split("T")[0]
+}
+
 export default function SingleMovie() {
   const [ready, setReady] = useState(false);
   const [movie, setMovie] = useState({});
@@ -40,8 +44,8 @@ export default function SingleMovie() {
       </div>
     );
 
-  const { id, trailer, poster, title, storyLine, director = {}, reviews = {}, writers = [] } = movie
-  return <div className="dark:bg-primary bg-white min-h-screen">
+  const { id, trailer, poster, title, storyLine, director = {}, reviews = {}, writers = [], cast = [], language, releseDate } = movie
+  return <div className="dark:bg-primary bg-white min-h-screen pb-10">
     <Container>
       <video poster={poster} controls src={trailer}></video>
       <div className="flex justify-between">
@@ -63,10 +67,28 @@ export default function SingleMovie() {
           <div className="space-x-2">
             {writers.map((w) => {
               return (
-                <p className="text-highlight dark:text-highlight-dark hover:underline cursor-pointer">{w.name}</p>
+                <p key={w.id} className="text-highlight dark:text-highlight-dark hover:underline cursor-pointer">{w.name}</p>
               )
             })}
           </div>
+        </div>
+        <div className="flex">
+          <p className="text-light-subtle dark:text-dark-subtle font-semibold mr-2">Cast:</p>
+          <div className="flex space-x-2">
+            {cast.map((c) => {
+              return c.leadActor ? (
+                <p key={c.profile.id} className="text-highlight dark:text-highlight-dark hover:underline cursor-pointer">{c.profile.name}</p>
+              ):null
+            })}
+          </div>
+        </div>
+        <div className="flex space-x-2">
+          <p className="text-light-subtle dark:text-dark-subtle font-semibold">Language:</p>
+          <p className="text-highlight dark:text-highlight-dark hover:underline cursor-pointer">{language}</p>
+        </div>
+        <div className="flex space-x-2">
+          <p className="text-light-subtle dark:text-dark-subtle font-semibold">Relese Date:</p>
+          <p className="text-highlight dark:text-highlight-dark hover:underline cursor-pointer">{convertDate(releseDate)}</p>
         </div>
       </div>
     </Container>
