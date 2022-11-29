81f06ebd531427de062349485e3c7371b462fcb3
Hoang Nam <namhoangde2002@gmail.com>
2022-11-30 00:46:23 +0700

    XTTN2-386 [Client] Refactoring Review Form

---
+++ frontend/src/components/form/RatingForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { AiOutlineStar, AiFillStar } from 'react-icons/ai'
+import Submit from './Submit'
+const ratings = new Array(10).fill("")
+export default function RatingForm({ busy, onSubmit }) {
+    const [selectedRatings, setSelectedRatings] = useState([])
+    const [content, setContent] = useState('')
+    const handleMouseEnter = (index) => {
+        const ratings = new Array(index + 1).fill("")
+        setSelectedRatings([...ratings])
+    }
+    const handleOnChange = ({ target }) => {
+        setContent(target.value)
+    }
+    const handleSubmit = () => {
+        if (!selectedRatings.length) return;
+        const data = {
+            ratings: selectedRatings.length,
+            content
+        }
+        onSubmit(data)
+    }
+    return (
+        <div>
+            <div className='p-5 dark:bg-primary bg-white rounded space-y-3'>
+                <div className='text-highlight dark:text-highlight-dark flex items-center relative'>
+                    <StarsOutlined onMouseEnter={handleMouseEnter} ratings={ratings} />
+                    <div className='flex items-center absolute top-1/2 -translate-y-1/2'>
+                    <StarsFilled onMouseEnter={handleMouseEnter} ratings={selectedRatings}  />
+                    </div>
+                </div>
+                <textarea value={content} onChange={handleOnChange} className='w-full h-24 border-2 p-2 dark:text-white text-primary rounded outline-none bg-transparent
+                resize-none'></textarea>
+                <Submit busy={busy} onClick={handleSubmit} value="Rate this movie" />
+            </div>
+        </div>
+    )
+}
+const StarsOutlined = ({ ratings, onMouseEnter }) => {
+    return ratings.map((_, index) => {
+        return (<AiOutlineStar onMouseEnter={() => onMouseEnter(index)} className="cursor-pointer" key={index} size={24} />)
+    })
+}
+
+const StarsFilled = ({ ratings, onMouseEnter }) => {
+    return ratings.map((_, index) => {
+        return <AiFillStar onMouseEnter={() => onMouseEnter(index)} className="cursor-pointer" key={index} size={24} />
+    })
+}
