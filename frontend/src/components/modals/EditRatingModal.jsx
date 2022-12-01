ac25ac0948a76dbf5aff67cb0f7813b8750518b1
pvq2k2 <vanquyet2392002@gmail.com>
2022-12-01 14:33:44 +0700

    XTTN2-416 [Client] Updating Review

--- frontend/src/components/modals/EditRatingModal.jsx
+++ frontend/src/components/modals/EditRatingModal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { updateReview } from "../../api/review";
 import { useNotification } from "../../hooks";
 import RatingForm from "../form/RatingForm";
 import ModalContainer from "./ModalContainer";
@@ -10,10 +10,16 @@ export default function EditRatingModal({
   onSuccess,
   onClose,
 }) {
-  const { movieId } = useParams();
   const { updateNotification } = useNotification();
 
-  const handleSubmit = async (data) => {};
+  const handleSubmit = async (data) => {
+    const { error, message } = await updateReview(initialState.id, data);
+    if (error) return updateNotification("error", error);
+
+    onSuccess({ ...data });
+    updateNotification("success", message);
+    onClose();
+  };
   return (
     <ModalContainer visible={visible} onClose={onClose} ignoreContainer>
       <RatingForm initialState={initialState} onSubmit={handleSubmit} />
