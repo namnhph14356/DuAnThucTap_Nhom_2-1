b735eed372c2c1625eb06051528e9ea5c879e89e
pvq2k2 <vanquyet2392002@gmail.com>
2022-12-02 22:34:08 +0700

    XTTN2-434 [Server] Changing Mail Trap WIth Sendin Gmail

--- backend/app.js
+++ backend/app.js
@@ -42,14 +42,6 @@ app.use(errorHandler);
 //   }
 // );
 
-// app.listen(8000, () => {
-//   console.log("the port is listening on port 8000");
-// });
-
-const uri = process.env.MONGO_URI;
-const db = uri || "mongodb://0.0.0.0:27017/review_app";
-mongoose.connect(db).then(() => console.log("Connect database successfully!"));
-// Content
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running port ${PORT}`);
